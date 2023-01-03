cdf893ba15db49b67734789f962cd1d285d7db49
parinyapt <parinyapt99@gmail.com>
2023-01-04 00:42:29 +0700

    edit index page add announcement

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,13 @@
     <PageIndexIntroVideo />
     <PageIndexSection2 />
 
-    <!-- <section class="px-4 mx-auto mt-12 mb-12 max-w-7xl sm:px-6 lg:px-4">
+    <section class="px-4 mx-auto mt-12 mb-12 max-w-7xl sm:px-6 lg:px-4">
       <article>
           <h2 class="text-2xl font-extrabold text-gray-900">{{ $t( 'page.index.section3.title' ) }}</h2>
           <div class="w-20 h-1 bg-blue-900 rounded"></div>
           <section class="grid mt-6 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
 
-              <article v-for="data in publicrelation" :key="data.id" class="relative overflow-hidden duration-200 transform bg-white rounded-lg shadow-lg group hover:shadow-2xl">
+              <!-- <article v-for="data in publicrelation" :key="data.id" class="relative overflow-hidden duration-200 transform bg-white rounded-lg shadow-lg group hover:shadow-2xl">
                   <div class="relative w-full h-80 md:h-64 lg:h-44">
                       <img :src="data.imgurl" :alt="data.title"
                           class="object-cover object-center w-full h-full">
@@ -26,25 +26,101 @@
                       </p>
 
                   </div>
+              </article> -->
+
+              <article class="relative overflow-hidden duration-200 transform bg-white rounded-lg shadow-lg group hover:shadow-2xl">
+                  <div class="relative w-full h-80 md:h-64 lg:h-44">
+                      <img src="ogimage.png"
+                          class="object-cover object-center w-full h-full">
+                  </div>
+                  <div class="px-3 py-4">
+                      <h3 class="pb-2 text-sm text-gray-500">
+                          <a class="px-2 py-1 text-white bg-blue-900 rounded-lg" href="#" target="_blank">
+                            <span class="absolute inset-0"></span>
+                            01/01/2023
+                          </a>
+                      </h3>
+                      <p class="text-base font-semibold text-gray-900 group-hover:text-blue-700">
+                        Announcement 1
+                      </p>
+
+                  </div>
+              </article>
+
+              <article class="relative overflow-hidden duration-200 transform bg-white rounded-lg shadow-lg group hover:shadow-2xl">
+                  <div class="relative w-full h-80 md:h-64 lg:h-44">
+                      <img src="ogimage.png"
+                          class="object-cover object-center w-full h-full">
+                  </div>
+                  <div class="px-3 py-4">
+                      <h3 class="pb-2 text-sm text-gray-500">
+                          <a class="px-2 py-1 text-white bg-blue-900 rounded-lg" href="#" target="_blank">
+                            <span class="absolute inset-0"></span>
+                            02/02/2023
+                          </a>
+                      </h3>
+                      <p class="text-base font-semibold text-gray-900 group-hover:text-blue-700">
+                        Announcement 2
+                      </p>
+
+                  </div>
+              </article>
+
+              <article class="relative overflow-hidden duration-200 transform bg-white rounded-lg shadow-lg group hover:shadow-2xl">
+                  <div class="relative w-full h-80 md:h-64 lg:h-44">
+                      <img src="ogimage.png"
+                          class="object-cover object-center w-full h-full">
+                  </div>
+                  <div class="px-3 py-4">
+                      <h3 class="pb-2 text-sm text-gray-500">
+                          <a class="px-2 py-1 text-white bg-blue-900 rounded-lg" href="#" target="_blank">
+                            <span class="absolute inset-0"></span>
+                            03/03/2023
+                          </a>
+                      </h3>
+                      <p class="text-base font-semibold text-gray-900 group-hover:text-blue-700">
+                        Announcement 3
+                      </p>
+
+                  </div>
+              </article>
+
+              <article class="relative overflow-hidden duration-200 transform bg-white rounded-lg shadow-lg group hover:shadow-2xl">
+                  <div class="relative w-full h-80 md:h-64 lg:h-44">
+                      <img src="ogimage.png"
+                          class="object-cover object-center w-full h-full">
+                  </div>
+                  <div class="px-3 py-4">
+                      <h3 class="pb-2 text-sm text-gray-500">
+                          <a class="px-2 py-1 text-white bg-blue-900 rounded-lg" href="#" target="_blank">
+                            <span class="absolute inset-0"></span>
+                            04/04/2023
+                          </a>
+                      </h3>
+                      <p class="text-base font-semibold text-gray-900 group-hover:text-blue-700">
+                        Announcement 4
+                      </p>
+
+                  </div>
               </article>
 
           </section>
       </article>
-    </section> -->
+    </section>
   </section>
 </template>
 
 <script>
-// export default {
-//   async asyncData({ $axios }) {
-//     var publicrelation = ''
-//     try {
-//       publicrelation = await $axios.$get('https://sheetdb.io/api/v1/w89np2de398ri');
-//     } catch (error) {
-//       alert(error);
-//     }
+export default {
+  // async asyncData({ $axios }) {
+  //   var publicrelation = ''
+  //   try {
+  //     publicrelation = await $axios.$get('https://sheetdb.io/api/v1/w89np2de398ri');
+  //   } catch (error) {
+  //     alert(error);
+  //   }
 
-//     return { publicrelation };
-//   }
-// }
+  //   return { publicrelation };
+  // }
+}
 </script>
